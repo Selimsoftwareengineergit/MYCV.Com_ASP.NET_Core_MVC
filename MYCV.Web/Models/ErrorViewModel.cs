9e3229ffdd04c86a3caad75468840ae1c5a469db
Selim <selim@dimikit.com>
2025-12-22 16:14:28 +0600

    Initial commit - Project setup

---
+++ MYCV.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYCV.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
