1db5d1177d886b8501a12694324b9a04f87d63ac
Selim <selim@dimikit.com>
2025-12-22 17:50:10 +0600

    Add web project

---
+++ MYCV.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MYCV.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthApiService _authApiService;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(IAuthApiService authApiService, ILogger<AccountController> logger)
+        {
+            _authApiService = authApiService;
+            _logger = logger;
+        }
+
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string? returnUrl = null)
+
+    }
+}
